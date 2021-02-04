6cb3db2a783842be420029df73dc228079f0216c
Sunconure11 <eyespot7b@aol.com>
2021-02-04 07:40:08 -0500

    Update CovetedMobs.java

--- src/main/java/net/sunconure11/covetedmobs/CovetedMobs.java
+++ src/main/java/net/sunconure11/covetedmobs/CovetedMobs.java
@@ -6,21 +6,29 @@ import net.fabricmc.api.ModInitializer;
 import net.sunconure11.covetedmobs.common.CMConfig;
 import net.sunconure11.covetedmobs.common.registry.CMEntityTypes;
 import net.sunconure11.covetedmobs.common.registry.CMWorldSpawns;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CovetedMobs implements ModInitializer {
+
 	public static final String MODID = "covetedmobs";
+	public static final Logger logger = LogManager.getLogger(MODID);
 	public static CMConfig config;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		AutoConfig.register(CMConfig.class, GsonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(CMConfig.class).getConfig();
 		CMEntityTypes.init();
 		CMWorldSpawns.init();
-		System.out.println("Hello Fabric world!");
+
+		logger.info("We need another and a wiser and perhaps a more mystical concept of animals.");
+		logger.info("Remote from universal nature, and living by complicated artifice, man in civilization surveys the creature through the glass of his knowledge and sees thereby a feather magnified and the whole image in distortion.");
+		logger.info("We patronize them for their incompleteness, for their tragic fate of having taken form so far below ourselves.");
+		logger.info("And therein we err, and greatly err.");
+		logger.info("For the animal shall not be measured by man.");
+		logger.info("In a world older and more complete than ours they moved finished and complete, gifted with extensions of the senses we have lost or never attained, living by voices we shall never hear.");
+		logger.info("They are not brethren, they are not underlings; they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.");
 	}
 }
